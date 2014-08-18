e548b18f223e2493472dcf393d21d1714d304216
Brylie Christopher Oxley <brylie@tuxfamily.org>
2014-08-19 01:12:50 +0300

    Convert place to integer, for list index

--- median.py
+++ median.py
@@ -4,9 +4,9 @@ def median(numbers):
     numbers.sort()
     
     if length % 2 == 0:
-        place = length / 2
+        place = int(length / 2) # index must be integer
         calculated_median = (numbers[place] + numbers[place - 1]) / 2.0
     else:
-        place = (length - 1) / 2
+        place = int((length - 1) / 2) # index must be integer
         calculated_median = numbers[place]
     return calculated_median
